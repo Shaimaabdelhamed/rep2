cdd052667165a2e159bb50a651bad2923ac4e720
Shaimaabdelhamed <shaimaa.abdelhamed.2014@gmail.com>
2016-07-19 21:33:26 +0200

    update to final code

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -11,16 +11,33 @@ namespace requiredtasks
     public partial class WebForm1 : System.Web.UI.Page
     {
         string path;
+        int counter=0;
+       // ViewState["counter"] = counter;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+
+                counter++;
+                ViewState["counter"] = counter;               
+            }
             Timer1.Tick += Timer1_Tick;
             path = Server.MapPath("~/");
+            Image1.ImageUrl = @"\imags\ho1.jpg";
+
         }
 
         private void Timer1_Tick(object sender, EventArgs e)
         {
+            counter = (int) ViewState["counter"];          
+          //  ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('"+ counter+"' )", true);          
             report rprt = new report();
-            using (StreamWriter w = File.AppendText(path + @"\log.txt"))
+            int count = (int) ViewState["counter"];
+            Label1.Text = count.ToString();
+            counter++;
+            ViewState["counter"]= counter;
+
+            using (StreamWriter w = File.AppendText(@path + @"\log.txt"))
             {
                 rprt.Writetxt("Hi", w);
                 //Server.MapPath("~/tmp/")
